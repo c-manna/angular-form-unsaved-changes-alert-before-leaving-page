567c7a7942c9790787ebb9fc61ad3be9cf6f3aa3
c-manna <cmanna7@gmail.com>
2022-02-23 11:34:54 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule, Routes } from '@angular/router';
+import { unSaveChangeGuard } from './guard/un-save-change.guard';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'hello',
+    pathMatch: 'full'
+  },
+  {
+    path: 'hello',
+    component: HelloComponent,
+    canDeactivate: [unSaveChangeGuard]
+  },
+];
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(routes) ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
